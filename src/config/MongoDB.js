ca49772f65812d856c37018b76b4a70f48ef3e0c
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-20 19:51:16 -0300

    refactor: :zap: refactor MongoDB.js

--- src/config/MongoDB.js
+++ src/config/MongoDB.js
@@ -2,5 +2,8 @@ import mongoose from 'mongoose';
 
 export const dbConnect = async () => {
 	const MONGODB_URL = process.env.MONGODB_URL;
-	mongoose.connect(MONGODB_URL);
+	mongoose
+		.connect(MONGODB_URL)
+		.then(console.log(`===> 🚀 Conectado a DB`))
+		.catch(err => console.log(`⚠️ Error en DB ===> ${err}`));
 };
