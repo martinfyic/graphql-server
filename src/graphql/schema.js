89b5a2a3b6f14036b08cda249d857a80bb9da918
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-20 22:06:04 -0300

    feat: :sparkles: create graphql schema

---
+++ src/graphql/schema.js
@@ -0,0 +1,32 @@
+import { buildSchema } from 'graphql';
+
+export const schema = buildSchema(`
+
+  type Query {
+    allProducts: [Product]
+    oneProduct: Product
+  }
+
+  type Mutation {
+    createProduct(input: ProductInput): Product
+    deleteProduct(id: ID!): Product
+    updateProduct(id: ID!, input: ProductInput): Product
+  }
+
+  type Product {
+    _id: ID
+    title: String
+    price: Float
+    thumbnail: String
+    description: String
+    stock: Int
+  }
+
+  input ProductInput {
+    title: String
+    price: Float
+    thumbnail: String
+    description: String
+    stock: Int
+  }
+`);
