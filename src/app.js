9471e0d5c29600e2aeaf2ab47204aa4db82f0695
Martin Ferreira <martin.f.yic@gmail.com>
2023-03-22 22:41:30 -0300

    feat: :zap: add graphql-tools for schema and use makeExecutableSchema

--- src/app.js
+++ src/app.js
@@ -1,41 +1,29 @@
 import 'dotenv/config';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import cors from 'cors';
 import { dbConnect } from './config/MongoDB.js';
-import { schema } from './graphql/schema.js';
-import ProductController from './controller/productController.js';
-
-const api = new ProductController();
+import { typeDef, resolver } from './graphql/index.js';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-	cors({
-		origin: '*',
-		methods: 'GET, POST, PUT, DELETE, OPTIONS',
-	})
-);
+app.use(cors());
+
+const schema = makeExecutableSchema({
+	typeDefs: typeDef,
+	resolvers: resolver,
+});
+
 app.use(
 	'/graphql',
 	graphqlHTTP({
 		schema: schema,
-		rootValue: {
-			allProducts: api.allProducts,
-			oneProduct: api.oneProduct,
-			createProduct: api.createProduct,
-			deleteProduct: api.deleteProduct,
-			updateProduct: api.updateProduct,
-		},
 		graphiql: true,
 	})
 );
-
-app.get('/', (req, res) => {
-	res.send('<h1> PROBANDO CONECTION ✅ </h1>');
-});
 
 dbConnect();
 
